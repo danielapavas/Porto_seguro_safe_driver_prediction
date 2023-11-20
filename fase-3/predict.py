e002110b8e382dec890f0d327caf5a06e34f4d7f
Daniela Pavas <75345956+danielapavas@users.noreply.github.com>
2023-11-20 07:49:41 -0500

    Add files via upload

---
+++ fase-3/predict.py
@@ -0,0 +1,107 @@
+<<<<<<< HEAD
+import argparse
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from loguru import logger
+import os
+import pandas as pd
+import pickle
+import joblib
+
+# Argumentos de línea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', required=True, type=str, help='test.csv')
+parser.add_argument('--predictions_file', required=True, type=str, help='predictions.csv')
+parser.add_argument('--model_file', required=True, type=str, help='model.pkl')
+
+args = parser.parse_args()
+
+model_file = args.model_file
+input_file = args.input_file
+predictions_file = args.predictions_file
+
+# Verificar si los archivos existen
+if not os.path.isfile(model_file):
+    logger.error(f"El archivo de modelo {model_file} no existe")
+    exit(-1)
+
+if not os.path.isfile(input_file):
+    logger.error(f"El archivo de entrada {input_file} no existe")
+    exit(-1)
+
+try:
+    # Cargar el modelo previamente entrenado
+    logger.info("Cargando el modelo")
+    model = joblib.load(model_file)
+
+    # Cargar el conjunto de datos de entrada desde un archivo CSV
+    logger.info("Cargando los datos de entrada")
+    input_data = pd.read_csv(input_file)
+
+    # Realizar predicciones
+    logger.info("Realizando predicciones")
+    predictions = model.predict(input_data)  # Utiliza el modelo para hacer predicciones en los datos de entrada
+
+    # Crear un DataFrame con las predicciones y las columnas adecuadas
+    output_df = pd.DataFrame({'Predicciones': predictions})
+
+    # Guardar las predicciones en un archivo CSV
+    logger.info(f"Guardando las predicciones en {predictions_file}")
+    output_df.to_csv(predictions_file, index=False)
+    logger.info("Predicciones guardadas con éxito.")
+except Exception as e:
+    logger.error(f"Ocurrió un error: {str(e)}")
+=======
+import argparse
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from loguru import logger
+import os
+import pandas as pd
+import pickle
+import joblib
+
+# Argumentos de línea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', required=True, type=str, help='test.csv')
+parser.add_argument('--predictions_file', required=True, type=str, help='predictions.csv')
+parser.add_argument('--model_file', required=True, type=str, help='model.pkl')
+
+args = parser.parse_args()
+
+model_file = args.model_file
+input_file = args.input_file
+predictions_file = args.predictions_file
+
+# Verificar si los archivos existen
+if not os.path.isfile(model_file):
+    logger.error(f"El archivo de modelo {model_file} no existe")
+    exit(-1)
+
+if not os.path.isfile(input_file):
+    logger.error(f"El archivo de entrada {input_file} no existe")
+    exit(-1)
+
+try:
+    # Cargar el modelo previamente entrenado
+    logger.info("Cargando el modelo")
+    model = joblib.load(model_file)
+
+    # Cargar el conjunto de datos de entrada desde un archivo CSV
+    logger.info("Cargando los datos de entrada")
+    input_data = pd.read_csv(input_file)
+
+    # Realizar predicciones
+    logger.info("Realizando predicciones")
+    predictions = model.predict(input_data)  # Utiliza el modelo para hacer predicciones en los datos de entrada
+
+    # Crear un DataFrame con las predicciones y las columnas adecuadas
+    output_df = pd.DataFrame({'Predicciones': predictions})
+
+    # Guardar las predicciones en un archivo CSV
+    logger.info(f"Guardando las predicciones en {predictions_file}")
+    output_df.to_csv(predictions_file, index=False)
+    logger.info("Predicciones guardadas con éxito.")
+except Exception as e:
+    logger.error(f"Ocurrió un error: {str(e)}")
+>>>>>>> 32194d37c6a25127ffbe316cc8754b2696069fe9
---
+++ fase-3/train.py
@@ -0,0 +1,93 @@
+<<<<<<< HEAD
+import argparse
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from loguru import logger
+import os
+
+# Argumentos de línea de comandos
+
+parser = argparse.ArgumentParser(description="Entrenar un modelo y guardar una nueva versión")
+parser.add_argument('--data_file', required=True, type=str, help='train.csv')
+parser.add_argument('--model_file', required=True, type=str, help='model.pkl')
+parser.add_argument('--overwrite_model', default=False, action='store_true', help='Si se establece, sobrescribe el archivo del modelo si ya existe')
+
+args = parser.parse_args()
+
+model_file = args.model_file
+data_file = args.data_file
+overwrite = args.overwrite_model
+
+if os.path.isfile(model_file) and not overwrite:
+    logger.info(f"El archivo del modelo {model_file} ya existe. Utilice la opción --overwrite_model para sobrescribirlo.")
+    exit(-1)
+
+try:
+    logger.info("Cargando los datos de entrenamiento")
+    train_data = pd.read_csv(data_file)
+    Xtr = train_data.values[:, :-1]  # Se asume que la última columna es la etiqueta y se excluye
+    ytr = train_data.values[:, -1]  # La última columna se asume como la etiqueta
+
+    logger.info("Entrenando el modelo")
+    model = RandomForestClassifier()
+    model.fit(Xtr, ytr)
+
+    logger.info(f"Modelo entrenado con éxito.")
+
+    # Calcula y muestra la precisión en los datos de entrenamiento
+    train_accuracy = model.score(Xtr, ytr)
+    logger.info(f"Precisión en los datos de entrenamiento: {train_accuracy:.2f}")
+
+    logger.info(f"Guardando el modelo en {model_file}")
+    joblib.dump(model, model_file)
+
+except Exception as e:
+    logger.error(f"Error al cargar los datos o entrenar el modelo: {str(e)}")
+=======
+import argparse
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from loguru import logger
+import os
+
+# Argumentos de línea de comandos
+
+parser = argparse.ArgumentParser(description="Entrenar un modelo y guardar una nueva versión")
+parser.add_argument('--data_file', required=True, type=str, help='train.csv')
+parser.add_argument('--model_file', required=True, type=str, help='model.pkl')
+parser.add_argument('--overwrite_model', default=False, action='store_true', help='Si se establece, sobrescribe el archivo del modelo si ya existe')
+
+args = parser.parse_args()
+
+model_file = args.model_file
+data_file = args.data_file
+overwrite = args.overwrite_model
+
+if os.path.isfile(model_file) and not overwrite:
+    logger.info(f"El archivo del modelo {model_file} ya existe. Utilice la opción --overwrite_model para sobrescribirlo.")
+    exit(-1)
+
+try:
+    logger.info("Cargando los datos de entrenamiento")
+    train_data = pd.read_csv(data_file)
+    Xtr = train_data.values[:, :-1]  # Se asume que la última columna es la etiqueta y se excluye
+    ytr = train_data.values[:, -1]  # La última columna se asume como la etiqueta
+
+    logger.info("Entrenando el modelo")
+    model = RandomForestClassifier()
+    model.fit(Xtr, ytr)
+
+    logger.info(f"Modelo entrenado con éxito.")
+
+    # Calcula y muestra la precisión en los datos de entrenamiento
+    train_accuracy = model.score(Xtr, ytr)
+    logger.info(f"Precisión en los datos de entrenamiento: {train_accuracy:.2f}")
+
+    logger.info(f"Guardando el modelo en {model_file}")
+    joblib.dump(model, model_file)
+
+except Exception as e:
+    logger.error(f"Error al cargar los datos o entrenar el modelo: {str(e)}")
+>>>>>>> 32194d37c6a25127ffbe316cc8754b2696069fe9
